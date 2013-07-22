a0f5bc518f9f73baf5e3954b37e44b55815be97e
App Academy Student <student@appacademy.io>
2013-07-22 12:01:01 -0700

    Added place_bombs, reveal, bomb_count methods

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,9 +1,26 @@
 class Board
   def initialize
     @board = []
-    81.times { @board << Tile.new }
+    81.times { @board << Tile.new() }
+    @bombs = 10
+  end
+
+  def place_bombs
+    arry = @board.combination(@bombs).sample
   end
 end
 
 class Tile
+  def initialize(flag, x, y, board, bomb)
+    @flag = flag
+    @pos = [x, y]
+    @board = board
+    @bomb = bomb
+  end
+
+  def reveal
+  end
+
+  def bomb_count
+  end
 end

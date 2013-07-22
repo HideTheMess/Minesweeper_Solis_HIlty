d5e0511e8f7e56664379b5ea1149b070c77c3daf
App Academy Student <student@appacademy.io>
2013-07-22 14:10:32 -0700

    Add neighbors

--- minesweeper.rb
+++ minesweeper.rb
@@ -6,21 +6,36 @@ class Board
   end
 
   def place_bombs
-    arry = @board.combination(@bombs).sample
+    arry = @board.combination(@bombs).sample # .entries.sample
   end
 end
 
 class Tile
-  def initialize(flag, x, y, board, bomb)
-    @flag = flag
+  def initialize(x, y, board, bomb)
+    @flag = false
     @pos = [x, y]
     @board = board
     @bomb = bomb
   end
 
   def reveal
+
+
+
+
+
+
+
+    neighbors.each { |neighbor| neighbor.reveal }
   end
 
   def bomb_count
+  end
+
+  def neigbors
+    diff_array = [[-1, 1], [0, 1], [1, 1], [-1, 0], [1, 0], [-1, -1], [0, -1], [1, -1]]
+
+    neigh_pos = diff_array.map { |x_move, y_move| [self.x + x_move, self.y + y_move] }
+    neigh_pos.map { |x, y| @board[x][y] }
   end
 end

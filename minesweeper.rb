83f58190ab95a5be7e4d98ca0835883d9aabd4f7
App Academy Student <student@appacademy.io>
2013-07-22 14:49:05 -0700

    Random improvements Pt 1

--- minesweeper.rb
+++ minesweeper.rb
@@ -1,35 +1,66 @@
 class Board
-  def initialize
-    @board = []
-    81.times { @board << Tile.new() }
-    @bombs = 10
+
+  def initialize(bomb_count = 10)
+    @board = populate_board
+    @bombs = bomb_count
+  end
+
+  def play
+  end
+
+  def populate_board
+    board = []
+    9.times do |x|
+      temp_array = []
+      9.times do |y|
+        temp_array << Tile.new(x, y, self) # Maybe '@board'?
+      end
+      board << temp_array
+    end
+    board
   end
 
   def place_bombs
-    arry = @board.combination(@bombs).sample # .entries.sample
+    (0..8).to_a.repeated_permutation(2).entries.shuffle[0...@bombs].each do |x, y|
+      @board[x][y].bomb = true
+    end
+  end
+
+  def print_board
+
   end
 end
 
 class Tile
-  def initialize(x, y, board, bomb)
+  attr_accessor :bomb
+
+  def initialize(x, y, board
     @flag = false
     @pos = [x, y]
     @board = board
-    @bomb = bomb
+    @bomb = false
+    @revealed = false
   end
 
   def reveal
 
+    if bomb
+      @board.gameover
+    end
 
 
 
 
 
-
-    neighbors.each { |neighbor| neighbor.reveal }
+    neighbors.each { |neighbor| neighbor.reveal } if no bomb or no bomb nearby
   end
 
   def bomb_count
+    neighbors.inject(0) { |accum, neigh| accum + 1 if neigh.bomb? }
+  end
+
+  def bomb?
+    @bomb
   end
 
   def neigbors
